038ab09592f2509ae6afc8a997ae4c6bbd1d1703
David W. McKelvey <david@mckelveycreative.com>
2018-11-09 17:37:05 -0800

    fixing <Link /> lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
   extends: 'react-tools',
+  rules: {
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: [ 'Link' ],
+        specialLink: [ 'to', 'hrefLeft', 'hrefRight' ],
+        aspects: [ 'noHref', 'invalidHref', 'preferButton' ],
+      },
+    ],
+  },
 }
