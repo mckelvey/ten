824d36d7aa16cbd00f263208981742621a76ee26
David W. McKelvey <david@mckelveycreative.com>
2018-11-09 17:24:06 -0800

    adding data and parser

---
+++ data/parser.mjs
@@ -0,0 +1,107 @@
+import { basename, dirname } from 'path';
+import { readdir, readFile, writeFile } from 'fs';
+import _ from 'lodash';
+import parser from 'fast-xml-parser';
+
+const __dirname = dirname(new URL(import.meta.url).pathname);
+const svgFolder = `${__dirname}/svg`;
+const jsonFolder = `${__dirname}`;
+
+const dateInRange = (dateString, yearMatcher) => {
+  if (!dateString) {
+    return false;
+  }
+  const [year, month, day] = dateString.split('-');
+  if (!yearMatcher.test(dateString)) {
+    return false;
+  }
+  if (year === '2008') {
+    if (month !== '12' || parseInt(day, 10) < 6) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const collectData = (jsObject, yearMatcher) => _.reduce(
+  jsObject.svg.g.g,
+  (result, g) => {
+    if (g.rect) {
+      _.forEach(g.rect, rect => {
+        if (dateInRange(rect['data-date'], yearMatcher)) {
+          result.push({
+            count: rect['data-count'],
+            date: rect['data-date'],
+          });
+        }
+      });
+    }
+    return result;
+  },
+  []
+);
+
+const data = {};
+
+readdir(svgFolder, (error, items) => {
+  if (error) {
+    throw error;
+  }
+  Promise.all(
+    _.map(
+      items,
+      filename => {
+        const fileYear = basename(filename, '.svg');
+        const yearMatcher = new RegExp(`^${fileYear}-`, '');
+        return new Promise((resolve, reject) => {
+          readFile(`${svgFolder}/${fileYear}.svg`, (error, XMLData) => {
+            if (error) {
+              reject(error);
+            }
+            const jsObject = parser.parse(
+              XMLData.toString(), {
+                attributeNamePrefix: '',
+                ignoreAttributes: false,
+              }
+            );
+            resolve(collectData(jsObject, yearMatcher));
+          });
+        });
+      }
+    ))
+    .then(data => {
+      const orderedData = _.sortBy(_.flatten(data), 'date');
+      const summary = {
+        commitCount: 0,
+        dayCount: 0,
+        firstDate: _.head(orderedData).date,
+        lastDate: _.last(orderedData).date,
+      };
+      const history = _.reduce(
+        orderedData,
+        (result, { count, date }) => {
+          const numericCount = parseInt(count, 10);
+          summary.commitCount += numericCount;
+          summary.dayCount += 1;
+          return {
+            ...result,
+            [date]: { count: numericCount, totalToDate: summary.commitCount },
+          };
+        },
+        {}
+      );
+      return { history, summary };
+    })
+    .then(data => {
+      writeFile(
+        `${jsonFolder}/commit-history.json`,
+        JSON.stringify(data),
+        error => {
+          if (error) {
+            throw error;
+          }
+          console.log('SVGs parsed successfully.');
+        }
+      );
+    });
+});
