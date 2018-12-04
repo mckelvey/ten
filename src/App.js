bbdeeeaa3e1deac1c24801cfd8368dd6f6c5daaa
David W. McKelvey <david@mckelveycreative.com>
2018-12-03 21:43:12 -0800

    basic vars

--- src/App.js
+++ src/App.js
@@ -8,18 +8,27 @@ import './App.scss';
 
 const Day = universal(import('./pages/Day'));
 
-const App = () => (
-  <Router>
-    <div>
-      <Head>
-        <link href="https://fonts.googleapis.com/css?family=Roboto:400,500|Zilla+Slab:500,700&amp;subset=latin-ext" rel="stylesheet" />
-      </Head>
-      <Switch>
-        <Route path="/day/:date" component={Day} />
-        <Routes />
-      </Switch>
-    </div>
-  </Router>
-);
+class App extends React.PureComponent {
+  componentWillMount() {
+    document.documentElement.style.setProperty('--color-primary-gradient-strong', 'var(--color-teal-strong)');
+    document.documentElement.style.setProperty('--color-primary-gradient-pale', 'var(--color-teal-pale)');
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Head>
+            <link href="https://fonts.googleapis.com/css?family=Roboto:400,500|Zilla+Slab:500,700&amp;subset=latin-ext" rel="stylesheet" />
+          </Head>
+          <Switch>
+            <Route path="/day/:date" component={Day} />
+            <Routes />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default hot(module)(App);
