3ce2c0e51d862dbb703e4bc323c7e6b9ac464049
David W. McKelvey <david@mckelveycreative.com>
2018-12-04 21:09:28 -0800

    pseudo-random coloration

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import shuffle from 'lodash/shuffle';
 import React from 'react';
 import { Head, Router, Route, Switch } from 'react-static';
 import universal from 'react-universal-component';
@@ -8,11 +9,31 @@ import './App.scss';
 
 const Day = universal(import('./pages/Day'));
 
+const randomInteger = maximum =>
+  Math.floor(Math.random() * Math.floor(maximum));
+
+const colors = shuffle([
+  'yellow',
+  'orange',
+  'teal',
+  'blue',
+]);
+
+const setProperty = (property, value) =>
+  document.documentElement.style.setProperty(property, value);
+
 class App extends React.PureComponent {
   componentWillMount() {
     if (typeof window !== 'undefined') {
-      document.documentElement.style.setProperty('--color-primary-gradient-strong', 'var(--color-teal-strong)');
-      document.documentElement.style.setProperty('--color-primary-gradient-pale', 'var(--color-teal-pale)');
+      setProperty('--angle-primary-gradient', `${randomInteger(359)}deg`);
+      setProperty('--angle-secondary-gradient', `${randomInteger(359)}deg`);
+      setProperty('--color-primary', `var(--color-${colors[0]})`);
+      setProperty('--color-primary-medium', `var(--color-${colors[0]}-medium)`);
+      setProperty('--color-primary-pale', `var(--color-${colors[0]}-pale)`);
+      setProperty('--color-primary-gradient-strong', `var(--color-${colors[1]}-strong)`);
+      setProperty('--color-primary-gradient-pale', `var(--color-${colors[1]}-pale)`);
+      setProperty('--color-secondary-gradient-strong', `var(--color-${colors[2]}-strong)`);
+      setProperty('--color-secondary-gradient-pale', `var(--color-${colors[2]}-pale)`);
     }
   }
 
